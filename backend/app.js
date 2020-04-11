e48c38ed52c2ea7e35eede9e5ec5435f8d6b37ca
Dweep Panchal <dweep.programmer@gmail.com>
2020-04-11 23:52:07 +0530

    Add DB and create user model
    User model is created and other models files are added

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected!"))
+  .catch(() => console.log("Failed to Connect DB"));
 
 app.get("/", (req, res) => {
   res.send("Server Testing");
 });
 
+// Server Connection
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server Running at Port ${port}`));
