fb11b44d04469743b34c64c71d5df52826f2e249
Dweep Panchal <dweep.programmer@gmail.com>
2020-04-28 17:36:24 +0530

    app.js changed

--- app.js
+++ app.js
@@ -45,18 +45,10 @@ app.use("/api", orderRoutes);
 app.use("/api", stripeRoutes);
 app.use("/api", braintreeRoutes);
 
-// app.use(express.static(path.join(__dirname, "client/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-// });
-
-const root = require("path").join(__dirname, "client", "build");
-
-app.use(express.static(root));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile("index.html", { root });
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // Server Connection
